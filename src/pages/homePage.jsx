c5c41613008a402569046cf49b54c809731e5fce
JordiPalacios <jordipalacioseso@gmail.com>
2024-04-04 21:20:36 +0200

    pages done

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -1,8 +1,25 @@
+import { NavPage } from "../components"
+
 export const HomePage = () => {
 
     return (
         <>
-            <h1>Página: Home</h1>
+            <nav>
+                <ul>
+                    <li>
+                        <NavPage navigateTo={'/'} label={'Home'} />
+                    </li>
+                    <li>
+                        <NavPage navigateTo={'/create-goal'} label={'Create Goal'} />
+                    </li>
+                    <li>
+                        <NavPage navigateTo={'/users-page'} label={'LogIn'} />
+                    </li>
+                </ul>
+            </nav>
+            <header>                
+                <h1>Home Goals App</h1>
+            </header>
         </>
     )
 }
