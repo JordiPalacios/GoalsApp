49f208c3dc524df80c1a873d96bbc3b365aae4c5
JordiPalacios <jordipalacioseso@gmail.com>
2024-04-05 07:03:08 +0200

    useCounter make it

--- src/hooks/useCounter.jsx
+++ src/hooks/useCounter.jsx
@@ -1,7 +1,14 @@
+import { useState } from "react"
 
-export const useCounter = () => {
+export const useCounter = (initialValue = 0) => {
+    const [value, setValue] = useState(initialValue)
 
-    return (
+    const increase = () => setValue(value + 1)
+    const decrease = () => setValue(value - 1)
 
-    )
+    if (value < 0) {
+        setValue(0)
+    }
+
+    return [ value, increase, decrease ]
 }
