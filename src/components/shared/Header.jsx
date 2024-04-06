4b050c5106ec54324d5ed4aefb750971c3d70b93
JordiPalacios <jordipalacioseso@gmail.com>
2024-04-06 13:54:37 +0200

    satructured and added icons with text

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,8 +1,21 @@
+import { NavPage } from "./NavPage"
+import "./styles/index.js"
+
 export const Header = () => {
     return (
-        <header>
-            <h1>Goals App</h1>
-            <h2>Logo: Inicio Sesion</h2>
+        <header className="headerContainer">
+            <div className="logoContent">
+                <img src="public/icons/rocket.svg" alt="rocket" className=" mr-2 h-10 w-10"/>
+                <a href="/" className="title"> Goals App </a>
+            </div>
+            <nav className="userContent">         
+                <NavPage 
+                    navigateTo={'/users-page'} 
+                    content={ 
+                        <img src="public/icons/user.svg" alt="userLogIn" className="h-6 w-6"/> 
+                    }
+                />      
+            </nav>
         </header>
     )
 }
