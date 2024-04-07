b814f8a6f0f3522ba4c53a52cdffddf25ddab3e0
JordiPalacios <jordipalacioseso@gmail.com>
2024-04-07 12:56:52 +0200

    created nav label and added link class

--- src/components/shared/NavPage.jsx
+++ src/components/shared/NavPage.jsx
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom"
 
 export const NavPage = ({ navigateTo, content }) => {
     return (
-        <>
+        <nav className="link">
             <Link to= { navigateTo } > { content } </Link>
-        </>
+        </nav>
     )
 }
