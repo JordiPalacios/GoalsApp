dcbe0e2fa62895ddae3e68333288a989054c8b86
JordiPalacios <jordipalacioseso@gmail.com>
2024-04-07 11:52:21 +0200

    footer content and css done

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -1,8 +1,9 @@
+
 export const Footer = () => {
 
     return (
-        <footer>
-            <small> © 2024 - Jordi Code </small>
+        <footer className="footerContainer">
+            <small> Created & Developed by Jordi Code. © 2024 All rights reserved  </small>
         </footer>
     )
 }
