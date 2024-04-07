82c47fe07ffac183f0a299e8dbf5dd68841747a6
JordiPalacios <jordipalacioseso@gmail.com>
2024-04-07 12:56:40 +0200

    nav label deleted and created aside waith navpage component

--- src/components/shared/NavbarMenu.jsx
+++ src/components/shared/NavbarMenu.jsx
@@ -2,18 +2,25 @@ import { NavPage } from "./NavPage"
 
 export const NavbarMenu = () => {
     return (
-        <nav>
-            <ul>
-                <li>
-                    <NavPage navigateTo={'/'} label={'Home'} />
-                </li>
-                <li>
-                    <NavPage navigateTo={'/create-goal'} label={'Create Goal'} />
-                </li>
-                <li>
-                    <NavPage navigateTo={'/users-page'} label={'LogIn'} />
-                </li>
-            </ul>
-        </nav>
+        <aside className="aside">
+            <NavPage 
+            navigateTo={'/'} 
+            content={
+                <a>
+                    <img src="public/icons/list.svg" alt="List" className="icon"/>
+                    <span> Goals List </span>
+                </a>
+                } 
+            />
+            <NavPage 
+            navigateTo={'/'} 
+            content={
+                <a>
+                    <img src="public/icons/plus.svg" alt="Plus" className="icon"/>
+                    <span> Create Goal </span>
+                </a>
+                } 
+            />
+        </aside>
     )
 }
