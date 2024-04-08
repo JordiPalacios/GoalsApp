455f360808e980bea79f560a9d9e8252ea659b49
JordiPalacios <jordipalacioseso@gmail.com>
2024-04-08 20:47:16 +0200

    icon component added to project

--- src/components/shared/NavbarMenu.jsx
+++ src/components/shared/NavbarMenu.jsx
@@ -1,35 +1,30 @@
 import { NavPage } from "./NavPage"
+import { IconText } from "./IconText"
 import styles from '../homePage/mainHome.module.css'
 
 export const NavbarMenu = () => {
     return (
         <aside className={styles.aside}>
-            <div className={styles.navItemsContainer}>
+            <div className= "w-full">
                 <NavPage 
                 navigateTo={'/'} 
                 content={
-                    <>
-                        <img src="public/icons/list.svg" alt="List" className={styles.icon}/>
-                        <span className={styles.text}> Goals List </span>
-                    </>
-                    } 
-                    />
+                    <IconText icon = {"public/icons/list.svg"} iconAlt = {"HomeSVG"} text={"Home"} children = {styles.icon}/>
+                } 
+                />
                 <NavPage 
                 navigateTo={'/create-goal'} 
                 content={
-                    <>
-                        <img src="public/icons/plus.svg" alt="Plus" className={styles.icon}/>
-                        <span className={styles.text}> Create Goal </span>
-                    </>
-                    } 
-                    />
+                    <IconText icon = {"public/icons/plus.svg"} iconAlt = {"PlusSVG"} text={"Create Goal"} children = {styles.icon}/>
+                } 
+                />
             </div>
             <div className={styles.rrssContainer}>
-                <a href="https://www.linkedin.com/in/jordi-palacios-g%C3%B3mez-529046164" target="_blank" rel="noreferrer">
-                    <img src="public/icons/linkedin.svg" alt="LinkedIn" className={styles.icon}/>
+                <a href="https://www.linkedin.com/in/jordi-palacios-g%C3%B3mez-529046164" target="_blank" rel="noreferrer" className={styles.rrssContent}>
+                    <IconText icon = {"public/icons/linkedin.svg"} iconAlt = {"LinkedInSVG"} text={<small>Jordi Palacios</small>} children = {styles.icon}/>
                 </a>
-                <a href="https://www.instagram.com/jordi_code?igsh=MXRvbmtiYmhheHRiZw%3D%3D&utm_source=qr" target="_blank" rel="noreferrer">
-                    <img src="public/icons/instagram.svg" alt="Instagram" className={styles.icon}/>
+                <a href="https://www.instagram.com/jordi_code?igsh=MXRvbmtiYmhheHRiZw%3D%3D&utm_source=qr" target="_blank" rel="noreferrer" className={styles.rrssContent}>
+                    <IconText icon = {"public/icons/instagram.svg"} iconAlt = {"InstagramSVG"} text={<small>@JordiCode</small>} children = {styles.icon}/>
                 </a>
             </div>
         </aside>
