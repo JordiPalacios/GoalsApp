bf2925ebe6ed4fdc94812e2f517dffb80afeed98
JordiPalacios <jordipalacioseso@gmail.com>
2024-04-09 20:26:01 +0200

    component for each goal created

--- src/components/goalsList/SingleGoal.jsx
+++ src/components/goalsList/SingleGoal.jsx
@@ -1,7 +1,32 @@
-export const SingleGoal = () => {
+import styles from './singleGoal.module.css'
+
+export const SingleGoal = ({ details, period, events, icon, iconAlt, goal, timesCompleted }) => {
+
     return (
-        <>
-            <h1> Single Goal</h1>
-        </>
+        <div className={styles.goalContainer + " card"}>
+            <div className='flex items-center'>
+                <div className={styles.icon}>
+                    <img src={icon} alt={iconAlt} />
+                </div>
+                <p className='text-xl ml-5 mr-10'> 
+                    {events} 
+                    <sub className='text-xs text-gray-500 ml-1'> 
+                        / {period} 
+                    </sub>
+                </p>
+                <p> {details} </p>
+            </div>
+            <div className='flex'>
+                <div className='relative m-2 mx-5'>
+                    <p className='text-center'> {timesCompleted} of {goal} </p>
+                    <div className={styles.navbarContainer}>
+                        <div className={styles.navbarProgress} 
+                        style={{ width: `${Math.round((timesCompleted / goal) * 100)}%`}}>                            
+                        </div>
+                    </div>
+                </div>
+                <button className='button button--gray'>Completed</button>
+            </div>
+        </div>
     )
 }
--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -1,11 +1,12 @@
 import { Footer, Header, MainHome } from "../components"
+import { ListGoal } from "../components/goalsList/ListGoal"
 
 export const HomePage = () => {
 
     return (
         <>
             <Header />
-            <MainHome children={' Test '}/>
+            <MainHome children={<ListGoal />} />
             <Footer />
         </>
     )
