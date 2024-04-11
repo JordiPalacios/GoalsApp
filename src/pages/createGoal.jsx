d531665f30c5a05d917857e098cf82ca767d92bd
JordiPalacios <jordipalacioseso@gmail.com>
2024-04-11 07:52:12 +0200

    routed correctly custom components

--- src/pages/createGoal.jsx
+++ src/pages/createGoal.jsx
@@ -1,10 +1,11 @@
 import {Header, Footer, MainHome} from "../components"
+import { GoalDetails } from "../components/newGoal/GoalDetails"
 
 export const CreateGoal = () => {
     return (
         <>
             <Header />
-                <MainHome />
+                <MainHome children={<GoalDetails />} />
             <Footer />
         </>
     )
--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -1,13 +1,12 @@
 import { Footer, Header, MainHome } from "../components"
 import { ListGoal } from "../components/goalsList/ListGoal"
-import { GoalDetails } from "../components/newGoal/GoalDetails"
 
 export const HomePage = () => {
 
     return (
         <>
             <Header />
-            <MainHome children={<GoalDetails />} />
+            <MainHome children={<ListGoal />} />
             <Footer />
         </>
     )
